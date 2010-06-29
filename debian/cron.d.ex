df5932455fee2fc47bc1794932d3be1d61b709dc
anpez <>
2010-06-29 15:07:41 +0000

    Deleted some unused files under debian directory.

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the lenmus package
-#
-0 4	* * *	root	[ -x /usr/bin/lenmus_maintenance ] && /usr/bin/lenmus_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(lenmus):needs="X11" section="Applications/Education"\
-  title="LenMus" command="/usr/bin/lenmus"
