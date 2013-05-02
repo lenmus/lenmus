f9cd7098123b18401b8b09fd19179489ab277453
Cecilio Salmeron <s.cecilios@gmail.com>
2013-05-02 18:03:53 +0200

    Final commit for LenMus Phonascus release 5.3.1

--- cmake-modules/FindLomse.cmake
+++ cmake-modules/FindLomse.cmake
@@ -45,30 +45,36 @@ set( LOMSE_LIBRARIES   "${LOMSE_LIBRARY}" )
 
 
 # ------------------------------------------------------------------------
-# Extract LOMSE_VERSION from lomse_version.h
+# Find installed version
 # ------------------------------------------------------------------------
 
-file(STRINGS ${LOMSE_INCLUDE_DIRS}/lomse_version.h LOMSE_VERSION_LIST)
-list (GET LOMSE_VERSION_LIST 5 MAJOR_LINE)
-list (GET LOMSE_VERSION_LIST 6 MINOR_LINE)
-list (GET LOMSE_VERSION_LIST 7 TYPE_LINE)
-list (GET LOMSE_VERSION_LIST 8 PATCH_LINE)
-string(REGEX REPLACE "\#define LOMSE_VERSION_MAJOR    " "" LOMSE_VERSION_MAJOR "${MAJOR_LINE}")
-string(REGEX REPLACE "\#define LOMSE_VERSION_MINOR    " "" LOMSE_VERSION_MINOR "${MINOR_LINE}")
-string(REGEX REPLACE "\#define LOMSE_VERSION_TYPE     " "" LOMSE_VERSION_TYPE "${TYPE_LINE}")
-string(REGEX REPLACE "\#define LOMSE_VERSION_PATCH    " "" LOMSE_VERSION_PATCH "${PATCH_LINE}")
+if (EXISTS "${LOMSE_INCLUDE_DIRS}/lomse_version.h")
 
-set( LOMSE_REVISION "0")
+    # Extract LOMSE_VERSION from lomse_version.h
+    file(STRINGS ${LOMSE_INCLUDE_DIRS}/lomse_version.h LOMSE_VERSION_LIST)
+    list (GET LOMSE_VERSION_LIST 5 MAJOR_LINE)
+    list (GET LOMSE_VERSION_LIST 6 MINOR_LINE)
+#    list (GET LOMSE_VERSION_LIST 7 TYPE_LINE)
+#    list (GET LOMSE_VERSION_LIST 8 PATCH_LINE)
+    string(REGEX REPLACE "\#define LOMSE_VERSION_MAJOR    " "" LOMSE_VERSION_MAJOR "${MAJOR_LINE}")
+    string(REGEX REPLACE "\#define LOMSE_VERSION_MINOR    " "" LOMSE_VERSION_MINOR "${MINOR_LINE}")
+#    string(REGEX REPLACE "\#define LOMSE_VERSION_TYPE     " "" LOMSE_VERSION_TYPE "${TYPE_LINE}")
+#    string(REGEX REPLACE "\#define LOMSE_VERSION_PATCH    " "" LOMSE_VERSION_PATCH "${PATCH_LINE}")
 
-#message ("major = '${LOMSE_VERSION_MAJOR}'") 
-#message ("minor = '${LOMSE_VERSION_MINOR}'") 
-#message ("type = '${LOMSE_VERSION_TYPE}'") 
-#message ("patch = '${LOMSE_VERSION_PATCH}'") 
-#message ("revision = '${LOMSE_REVISION}'") 
+#    set(LOMSE_LIB_VERSION "${LOMSE_VERSION_MAJOR}.${LOMSE_VERSION_MINOR}.${LOMSE_VERSION_PATCH}" )
+    set(LOMSE_LIB_VERSION "${LOMSE_VERSION_MAJOR}.${LOMSE_VERSION_MINOR}" )
 
-set(LOMSE_LIB_VERSION "${LOMSE_VERSION_MAJOR}.${LOMSE_VERSION_MINOR}.${LOMSE_VERSION_PATCH}" )
+else()
 
+    #try with pkg-config
+    pkg_search_module(LOMSE_PKG liblomse)
+    if (LOMSE_PKG_FOUND)
+        set(LOMSE_LIB_VERSION ${LOMSE_PKG_VERSION})
+    else()
+        set(LOMSE_LIB_VERSION "unknown")
+    endif()
 
+endif()
 
 # handle the QUIETLY and REQUIRED arguments and set LOMSE_FOUND to TRUE if 
 # all listed variables are TRUE
@@ -76,7 +82,4 @@ INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Lomse  DEFAULT_MSG  LOMSE_LIBRARY  LOMSE_INCLUDE_DIRS)
 
 MARK_AS_ADVANCED(LOMSE_LIBRARY LOMSE_INCLUDE_DIR)
-
-#MESSAGE ( "LOMSE_INCLUDE_DIRS => ${LOMSE_INCLUDE_DIRS}" )
-#MESSAGE ( "LOMSE_LIBRARIES => ${LOMSE_LIBRARIES}" )
 
